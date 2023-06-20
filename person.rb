65e4d4cd72efd110d2de863fb92b8e73fde1d057
otegaa <ekiugbotega@gmail.com>
2023-06-20 15:42:57 +0200

    Define method initialize

---
+++ person.rb
@@ -0,0 +1,12 @@
+class Person
+  attr_reader :id
+  attr_accessor :name
+  attr_accessor :age
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
