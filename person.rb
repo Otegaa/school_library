5b7190c6d6d0ceb1797393ebfd733ddc1df6c36a
otegaa <ekiugbotega@gmail.com>
2023-06-20 16:02:49 +0200

    Add method initialize

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :age, :name
 
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,13 +10,12 @@ class Person
   end
 
   def can_use_services?
-    of_age? || @parent_permission 
+    of_age? || @parent_permission
   end
 
   private
 
   def of_age?
-   @age >= 18 
+    @age >= 18
   end
-
 end
