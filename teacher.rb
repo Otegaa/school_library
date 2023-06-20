e7cd4aa13c8a96e11f31347508ad513fe46a2d23
otegaa <ekiugbotega@gmail.com>
2023-06-20 16:16:50 +0200

    Add method initialize

---
+++ teacher.rb
@@ -0,0 +1,9 @@
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super (age, name, parent_permission)
+    @specialization = specialization
+  end
+
+end
