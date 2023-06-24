bb7c64358b2b680602e8a9fc873c7dad3a213dd9
otegaa <ekiugbotega@gmail.com>
2023-06-24 16:28:25 +0200

    Create the App class and implement

--- person.rb
+++ person.rb
@@ -1,6 +1,10 @@
 require_relative './name'
 
 class Person < Nameable
+   # getters
+  attr_reader :id, :parent_permission, :rentals
+  attr_accessor :age, :name, :rentals
+  
   # constructor
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -11,10 +15,6 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
-
-  # getters
-  attr_reader :id
-  attr_accessor :age, :name, :rentals
 
   # public methods
 
