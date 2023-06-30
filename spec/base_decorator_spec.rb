db35fb34a24b8c0cc9853fc160aca37bb9c00e8a
touredri <dcompte223@gmail.com>
2023-06-30 22:35:10 +0000

    test added for decorate part class

---
+++ spec/base_decorator_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../name'
+require_relative '../base_decorator'
+
+describe BaseDecorator do
+  let(:nameable) { double('nameable') }
+  let(:decorator) { BaseDecorator.new(nameable) }
+  # let(:name) { Name.new('John Doe') }
+  describe '#correct_name' do
+    it 'calls correct_name method on the nameable object' do
+      expect(nameable).to receive(:correct_name)
+      decorator.correct_name
+    end
+  end
+end
