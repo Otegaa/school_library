6bf90c6ca5172ca877017a6939f61ed35399dd58
touredri <dcompte223@gmail.com>
2023-07-01 02:43:59 +0000

    added appropriate methode in Solver class

--- solver.rb
+++ solver.rb
@@ -1,3 +1,19 @@
-
 class Solver
+  def factorial(number)
+    raise 'Number cannot be negative' if number.negative?
+
+    (1..number).reduce(:*) || 1
+  end
+
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 15).zero?
+    return 'buzz' if (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+
+    number
+  end
 end
